87d537cfd787f91268d1a37643493f8f448e663f
FedericoBustaffa <kiko.bustaffa@gmail.com>
2023-02-08 00:07:11 +0100

    liste

--- py_core.py
+++ py_core.py
@@ -1,3 +1,23 @@
 
+def fair_coin():
+    sample_space = {"heads", "tails"}
+    probability_heads = 1 / len(sample_space)
+    print(f"Probability of chosen heads is {probability_heads}")
+
+
+def is_heads(face):
+    return face == "Heads"
+
+
+def match_event(condition, sample_space):
+    p = len([sample for sample in sample_space if condition(sample)])
+    return p / len(sample_space)
+
+
 if __name__ == "__main__":
-    pass
+    fair_coin()
+    coin = {"Heads", "Tails"}
+    print(match_event(is_heads, coin))
+
+    l = [x for x in {1, 2, 3, 4, 5} if x % 2 != 0]
+    print(l)
