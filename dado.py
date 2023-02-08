3aeccc4fc59827c8468245357787a6ecad5795ed
FedericoBustaffa <kiko.bustaffa@gmail.com>
2023-02-08 17:24:21 +0100

    dado

--- dado.py
+++ dado.py
@@ -1,13 +1,14 @@
 from itertools import product
 
 
-def get_weigthed_sample_space(sample_space):
-    sums = [sum(s) for s in sample_space if True]
-    weigthed_sample_space = dict()
-    for s in sums:
-        weigthed_sample_space[s] += 1
+def get_weighted_sample_space(sample_space):
+    weighted_sample_space = {sum(outcome): 0 for outcome in sample_space}
 
-    return weigthed_sample_space
+    for outcome in sample_space:
+        total = sum(outcome)
+        weighted_sample_space[total] += 1
+
+    return weighted_sample_space
 
 
 def get_matching_event(condition, sample_space):
@@ -21,8 +22,12 @@ def compute_event_probability(samples, sample_space):
 if __name__ == "__main__":
     dado = list(range(1, 7))
 
-    sample_space = get_weigthed_sample_space(set(product(dado, repeat=6)))
+    sample_space = set(product(dado, repeat=6))
     samples = get_matching_event(lambda x: sum(x) == 21, sample_space)
     probability = compute_event_probability(samples, sample_space)
+    print(f"Probability of getting 21: {probability}")
 
+    weighted_sample_space = get_weighted_sample_space(
+        set(product(dado, repeat=6)))
+    probability = weighted_sample_space[21] / len(sample_space)
     print(f"Probability of getting 21: {probability}")
